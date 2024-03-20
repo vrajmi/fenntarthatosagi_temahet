81f76743344d22426b2ebc0537e7d6c24416ecea
davidrajkai <rajkai.david@liveedu.mathiasz.hu>
2024-03-20 18:26:11 +0100

    h

--- js/main.js
+++ js/main.js
@@ -149,6 +149,139 @@ function answer3() {
 }
 
 function endScreen() {
-  document.getElementById("stylee").innerHTML = ""; //Ebbe rkhatsz plusz csst amit szeretnél. Ha az alap csst akarod használni amit rajmund összerakott akkor hagy így mert így törli ki azt amit én raktam össze.
-  a.innerHTML = "" + sum + ""; //Ide kell beraknod azt hogy mit szeretnél látni a vége résznél
+  document.getElementById("stylee").innerHTML = "body {" +
+  "background-color: #1a3331" +
+  "color: #88a27e" +
+  "overflow-x: hidden" +
+  "overflow-y: hidden" +
+  "min-height: 100vh" +
+  "display: flex" +
+  "flex-direction: column" +
+  "}" +
+
+  ".brand {" +
+  "margin: 30px" +
+  "}" +
+
+  ".nav-brand-text {" +
+  "color: #88a27e" +
+  "font-family: 'Poppins', sans-serif" +
+  "font-weight: 900" +
+  "font-style: normal" +
+  "}" +
+
+  ".play-button {" +
+  "background-color: #188b34" +
+  "border-width: 0px" +
+  "border-radius: 50%" +
+  "width: 80px" +
+  "height: 80px" +
+  "}" +
+
+  ".corner-img {" +
+  "position: fixed" +
+  "right: -150px" +
+  "bottom: -150px" +
+  "}" +
+
+  "main {" +
+  "font-family: 'Poppins', sans-serif" +
+  "font-weight: 3002" +
+  "font-style: normal" +
+  "margin: 15px" +
+  "}" +
+
+  ".box {" +
+  "background: linear-gradient(to right, #88a27e 4px, transparent 4px) 0 0" +
+  "linear-gradient(to left, #88a27e 4px, transparent 4px) 100% 100%" +
+  "linear-gradient(to bottom, #88a27e 4px, transparent 4px) 0 0" +
+  "linear-gradient(to top, #88a27e 4px, transparent 4px) 100% 100%" +
+
+  "background-repeat: no-repeat" +
+  "background-size: 20px 20px" +
+  "padding: 10px" +
+  "}" +
+
+  "footer {" +
+  "margin-top: auto" +
+  "font-family: 'Poppins', sans-serif" +
+  "font-weight: 200" +
+  "font-style: normal" +
+  "}" +
+
+  "@media (max-width: 320px) {" +
+  ".corner-img {" +
+  "display: none" +
+  "}" +
+  "}" +
+
+  "@media (max-width: 767px) {" +
+  "body {" +
+  "overflow-y: scroll" +
+  "}" +
+
+  ".corner-img {" +
+  "position: fixed" +
+  "right: -200px" +
+  "bottom: -200px" +
+  "}" +
+  "}" +
+
+  "@media (max-width: 1023px) {" +
+  ".corner-img {" +
+  "display: none" +
+  "}" +
+  "}" +
+
+  ".animate {" +
+  "position: relative" +
+  "animation-name: animate" +
+  "animation-duration: 1s" +
+  "}" +
+
+  "@keyframes animate {" +
+  "from {" +
+  "bottom: -10px" +
+  "opacity: 0.5" +
+  "}" +
+
+  "to {" +
+  "bottom: 0" +
+  "opacity: 1" +
+  "}" +
+  "}" +
+
+  "backtothemain {" +
+  "background-color: rgba(26, 51, 49, 0.8)" +
+  "border-radius: 10px" +
+  "opacity: 0.7" +
+  "cursor: pointer" +
+  "color: white" +
+  "padding: 1em";
+  
+  //Ebbe rkhatsz plusz csst amit szeretnél. Ha az alap csst akarod használni amit rajmund összerakott akkor hagy így mert így törli ki azt amit én raktam össze.
+
+  a.innerHTML =  '<div class="row">' +
+  '<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center">' +
+  '<img src="img/floating_burger.png" class="img-fluid" alt="Hamburger kép" />' +
+  "</div>" +
+  '<div class="col-lg-8 col-md-6 col-sm-12 d-flex align-items-center text-center">' +
+  '<p class="box" id="here">' +
+
+  "</p>" +
+  '<p class="box">' +
+  "Légy tudatos, egy életünk van. Javasoljuk, hogy a gyorsan felszívódó szénhidrátokat (vagy épp a szupergyorsan felszívódókat) -mint a nassok, sütemények, krumpli, rizs- cseréld lassan felszívódó szénhidrátokra – basmati rizs, hajdina, köles, kuszkusz- és fogyassz elég folyadékot. Minden nap legalább egy 4km-es távot sétálj le gyorssétával. Ha azt érzed, hogy nehézkes az alvás, akkor lefekvés előtt egy 30 perccel már ne nézz tv-t és ne használd a telefonodat sem. Így nyugodtabb lesz az alvásod és másnap nem kelsz fáradtan, ami miatt amúgy összezabálsz mindent." +
+  "</p>" +
+  "</div>";
+   //Ide kell beraknod azt hogy mit szeretnél látni a vége résznél
+
+  if (13 <= sum <= 21) {
+    document.getElementById("here").innerHTML = "Gratulálunk, te tudod, hogy kell igazán egészségesen élni. Ami nagyon fontos, hogy továbbra is figyelj oda a megfelelő hidratálásra és a rostbevitelre. Ha még nem próbáltad, akkor itt az ideje kipróbálni az alternatív fehérje megoldásokat is. Szuper egészséges és finom tud lenni. Egyre vigyázz, azért ne hajtsd túl magad. ;)";
+  }
+  if (22 <= sum <= 30) {
+    document.getElementById("here").innerHTML = "Jó úton jársz, de még van mit javítani az étkezéseden. Figyelj a rost és a megfelelő fehérje bevitelre (hal, pulyka vagy csirke legyen a fő és a hüvelyes zöldségek). Nézz utána a mediterrán étrendnek, a tested meg fogja hálálni. A nassolást, amennyire lehet, mellőzd. A nyugodt alváshoz pedig elengedhetetlen a jó környezet, a sötét szoba. Nyugi, nincs szörny az ágy alatt. ;)"
+  }
+  else {
+    document.getElementById("here").innerHTML = "Ajjaj, nagy a baj. Nem figyelsz az étkezésedre. Ha ezen nem változtatsz, komoly egészségügyi következményei is lehetnek (mint a cukorbetegség, a magas vérnyomás vagy a korai csontritkulás)."
+  }
 }
